82f596983cba2d564a2a928daf671758338e005a
Caique Azevedo <dev.caique@gmail.com>
2023-08-21 23:16:45 -0300

    correções em getRepositoryList

--- theMagic.js
+++ theMagic.js
@@ -6,7 +6,7 @@ const VISIBILITY = {
 const STATUS = {
     PUBLIC: 'public',
     PRIVATE: 'private',
-    ARCHIVED: 'archived'
+    ARCHIVED: 'Private archive'
 }
 
 const repository = {
@@ -19,16 +19,18 @@ var repositoryList = [];
 var hideList = [];
 var isVisible = false;
 
-window.onload = function() {
+window.onload = function () {
     handleVisible();
+    getRepositoryList();
 };
 
 function hadleClick(action) {
-    if (repositoryList.length == 0) {
-        getRepositoryList();
+    action == VISIBILITY.HIDE ? hide() : unhide();
+
+    if (action == 'hide') {
+        hide()
     }
 
-    action == VISIBILITY.HIDE ? hide() : unhide();
     handleVisible();
 }
 
@@ -58,30 +60,29 @@ function getRepositoryList() {
     repoList.forEach(repo => {
         try {
             let tempRepo = repository;
-            tempRepo.status = () => {
-                if (repo.childNodes[1].childNodes[1].childNodes[3].childNodes[4].innerHTML) {
-                    return repo.childNodes[1].childNodes[1].childNodes[3].childNodes[4].innerHTML;
-                }
-            };
-            tempRepo.id = () => {
-                if (repo.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].innerHTML) {
-                    return repo.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].innerHTML;
-                }
+            if (repo.childNodes[1].childNodes[1].childNodes[3].childNodes[4].innerHTML) {
+                tempRepo.status = repo.childNodes[1].childNodes[1].childNodes[3].childNodes[4].innerHTML
             }
+            if (repo.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].innerHTML) {
+                tempRepo.id = repo.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].innerHTML;
+            }
+
             repositoryList.push(tempRepo);
             tempRepo.status === STATUS.ARCHIVED ? hideList.push(tempRepo) : '';
+            console.log('hide list: ', hideList);
+            console.log('repository list: ', repositoryList);
         } catch (error) {
             console.error(error);
         }
     })
 }
 
-function handleVisible(){
+function handleVisible() {
     const hide = document.getElementById('hide');
     const unhide = document.getElementById('unhide');
-    isVisible = !isVisible; 
+    isVisible = !isVisible;
 
-    if(isVisible) {
+    if (isVisible) {
         hide.checked = false;
         unhide.checked = true;
     } else {
