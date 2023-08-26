f6aace628935ce45e4cac61e590be32807e3c0c0
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2023-08-26 14:05:51 -0300

    boa sorte caicao monstro sagrado

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # plugin-githubarchivedteams
+
+chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], function (storage) {
+      console.log("token: ", storage);
+  });
--- github-service.js
+++ github-service.js
@@ -1,9 +1,31 @@
-function getCurrentTab() {
-    console.log('#######################################################################################################################################')
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = chrome.tabs.query(queryOptions);
+const token = chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], function (storage) {
+  console.log("token: ", storage);
+});
 
-    console.log(tab)
-    return tab;
+//async function getToken() {
+//  return await chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], function (storage) {
+//    console.log("token: ", storage);
+//    return storage;
+//  });
+//}
+
+const currentUrl = window.location.href;
+const regexOrg = /\/orgs\/(.*?)\/teams\//;
+const regexTeam = /\/teams\/(.*?)\//;
+
+const matchOrg = currentUrl.match(regexOrg);
+const matchTeam = currentUrl.match(regexTeam);
+
+const nameOrg = matchOrg[1]
+const nameTeam = matchTeam[1]
+
+console.log(matchOrg[1]);
+console.log(matchTeam[1]);
+
+const response = fetch(`https://api.github.com/orgs/${nameOrg}/teams/${nameTeam}/repos`, {
+  method: "GET",
+  headers: {
+    "Authorization": "token " + token,
+    "X-GitHub-Api-Version": "2022-11-28"
   }
+}).then(res => console.log(res))
