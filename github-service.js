110d7566c348e2fde70862244bf4f25f69bf7c14
Caique Azevedo <dev.caique@gmail.com>
2023-08-26 14:49:39 -0300

    resolvido os bugs do Gabriel

--- github-service.js
+++ github-service.js
@@ -1,14 +1,3 @@
-const token = chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], function (storage) {
-  console.log("token: ", storage);
-});
-
-//async function getToken() {
-//  return await chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], function (storage) {
-//    console.log("token: ", storage);
-//    return storage;
-//  });
-//}
-
 const currentUrl = window.location.href;
 const regexOrg = /\/orgs\/(.*?)\/teams\//;
 const regexTeam = /\/teams\/(.*?)\//;
@@ -19,13 +8,35 @@ const matchTeam = currentUrl.match(regexTeam);
 const nameOrg = matchOrg[1]
 const nameTeam = matchTeam[1]
 
-console.log(matchOrg[1]);
-console.log(matchTeam[1]);
+window.onload = function() {
+  getRepoList();
+};
 
-const response = fetch(`https://api.github.com/orgs/${nameOrg}/teams/${nameTeam}/repos`, {
-  method: "GET",
-  headers: {
-    "Authorization": "token " + token,
-    "X-GitHub-Api-Version": "2022-11-28"
-  }
-}).then(res => console.log(res))
+async function getToken() {
+  return await new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.get(["GITHUB_AUTHORIZATION"], res => {
+        resolve(res);
+      });
+
+    } catch (error) {
+      reject(console.error('erro: ', error))
+    }
+  })
+}
+
+async function getRepoList() {
+  let token = await getToken().then(token => {
+    return token.GITHUB_AUTHORIZATION;
+  });
+
+  let response = await fetch(`https://api.github.com/orgs/${nameOrg}/teams/${nameTeam}/repos`, {
+    method: "GET",
+    headers: {
+      "Authorization": `token ${token}`,
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
+  }).then(res => console.log(res))
+
+  console.log(response)
+}
