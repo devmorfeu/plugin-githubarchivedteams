db7ad0d4d4f9775c3c861a6ea7c1117ebebcf4ff
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2023-08-27 19:21:29 -0300

    feat: add toggle de on off no popup

--- src/github/github.js
+++ src/github/github.js
@@ -136,9 +136,28 @@ function buildHtml() {
               </div>
           </div>
       </div>
+      
       <div class="table-list-cell f5 text-center">
-
-      </div>
+      <span class="tooltipped tooltipped-s" aria-label="Past year of activity">
+         <svg width="155" height="30">
+            <defs>
+               <linearGradient id="gradient-683547609" x1="0" x2="0" y1="1" y2="0">
+                  <stop offset="0%" stop-color="var(--color-calendar-graph-day-L1-bg)"></stop>
+                  <stop offset="10%" stop-color="var(--color-calendar-graph-day-L2-bg)"></stop>
+                  <stop offset="25%" stop-color="var(--color-calendar-graph-day-L3-bg)"></stop>
+                  <stop offset="50%" stop-color="var(--color-calendar-graph-day-L4-bg)"></stop>
+               </linearGradient>
+               <mask id="sparkline-683547609" x="0" y="0" width="155" height="28">
+                  <polyline transform="translate(0, 28) scale(1,-1)" points="0,1 3,1 6,1 9,1 12,1 15,1 18,1 21,1 24,1 27,1 30,1 33,1 36,1 39,1 42,1 45,1 48,1 51,1 54,1 57,1 60,1 63,1 66,1 69,1 72,1 75,1 78,1 81,1 84,1 87,1 90,1 93,1 96,1 99,1 102,1 105,1 108,1 111,1 114,1 117,1 120,1 123,1 126,1 129,1 132,1 135,1 138,1 141,1 144,1 147,1 150,1 153,2 " fill="transparent" stroke="#8cc665" stroke-width="2">
+                  </polyline>
+               </mask>
+            </defs>
+            <g transform="translate(0, -12)">
+               <rect x="0" y="-2" width="155" height="30" style="stroke: none; fill: url(#gradient-683547609); mask: url(#sparkline-683547609)"></rect>
+            </g>
+         </svg>
+      </span>
+   </div>
 
       <div class="table-list-cell team-buttons pr-3 f5 text-right">
           <div class="permission-level-cell">
